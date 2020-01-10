b052532c0514475f8f3bc018adcbb3ac7e613d1e
Michael Hayworth <54606648+andrew-hayworth22@users.noreply.github.com>
2020-01-10 13:45:39 -0500

    Updated ticTacToe.cpp
    Implemented player key at the top of each print.

--- ticTacToe.cpp
+++ ticTacToe.cpp
@@ -10,8 +10,8 @@
 char board[9] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
 int moveCount = 0;
 
-bool didWin();
-void stop();
+bool stop();
+void print();
 
 int main() {
     int player = 0;
@@ -85,6 +85,7 @@ bool stop() {
 }
 
 void print() {
+    std::cout "Player One(X) | Player Two(O)" << std::endl;
     std::cout << "   |   |   " << std::endl;
     std::cout << " " << board[0] << " | " << board[1] << " | " << board[2] << std::endl;
     std::cout << "___|___|___" << std::endl;
@@ -93,5 +94,5 @@ void print() {
     std::cout << "___|___|___" << std::endl;
     std::cout << "   |   |   " << std::endl;
     std::cout << " " << board[6] << " | " << board[7] << " | " << board[8] << std::endl;
-    std::cout << "   |   |   " << std::endl;
+    std::cout << "   |   |   " << std::endl << std::endl;
 }
